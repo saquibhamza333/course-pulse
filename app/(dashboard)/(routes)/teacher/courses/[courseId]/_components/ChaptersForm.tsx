95ab95b49b78616dbef02efb744e709837983309
saquibhamza333 <saquibhamza333@gmail.com>
2024-04-30 16:31:02 +0530

    edit chapter functionality added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
@@ -15,13 +15,14 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Chapter, Course } from "@prisma/client";
+import { ChaptersList } from "./ChaptersList";
 
 interface ChaptersFormProps {
   initialData : Course & {chapters :Chapter[]};
@@ -32,7 +33,7 @@ const formSchema = z.object({
   title : z.string().min(1),
 })
 
-const ChaptersForm = ({initialData , courseId}:ChaptersFormProps) => {
+const ChaptersForm:React.FC<ChaptersFormProps>  = ({initialData , courseId}:ChaptersFormProps) => {
     
 const router = useRouter();
     const [isCreating,setIsCreating] = useState(false);
@@ -62,8 +63,35 @@ const router = useRouter();
             toast.error("Something went wrong")
         }
     }
+
+      const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData
+      });
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  }
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  }
+
     return ( 
-        <div className="mt-6 border  bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4 dark:bg-gray-800">
+
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
 
@@ -121,6 +149,7 @@ const router = useRouter();
           !initialData.chapters.length && "text-slate-500 italic"
         )}>
     {!initialData.chapters.length && "No chapters"}
+    <ChaptersList onEdit={onEdit} onReorder={onReorder} items={initialData.chapters || []}  />
   
      
     </div>
